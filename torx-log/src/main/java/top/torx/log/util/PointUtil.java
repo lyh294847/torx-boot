18fd4f8476ec6bcb3ea216c9f620218267eacd11
liuyuhua <294847294@qq.com>
2024-12-23 19:20:39 +0800

    1

---
+++ torx-log/src/main/java/top/torx/log/util/PointUtil.java
@@ -0,0 +1,12 @@
+package top.torx.log.util;
+
+/**
+ * @Author: LiuYuHua
+ * @Date: 2024/12/23 19:18
+ */
+public class PointUtil {
+
+    public static void error(String msg, Object extraInfo) {
+
+    }
+}
